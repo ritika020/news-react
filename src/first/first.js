87092b59c7230c944e2ee65d16e03f558dd690ee
Ritika India <rindia@atlassian.com>
2020-11-18 00:45:18 +0530

    first

---
+++ src/first/first.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import "./first.css"
+
+
+import pic from '../pic1.png';
+
+
+class first extends React.Component{
+    render()
+    {
+        return(
+
+
+            <div className="box"  >
+                <div className="imgBox"  >
+                <img  src={pic} className="pic"></img>
+                </div>
+                <div className="content"   >
+                <h1>The Election Will Bring a Hurricane of Misinformation</h1>
+            <h3>Here's how to prepare yourself for the disaster online.</h3>
+            <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.Amet minim mollit Exercitation veniam consequat sunt nostrud amet.Amet minim mollit. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet Amet minim mollit Exercitation veniam consequat sunt nostrud amet. Amet minim molitAmet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit Exercitation veniam consequat sunt nostrud amet Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.</p>
+            <div className="end"> 4th April, 2020 09:30 AM</div>
+</div>
+</div>
+
+                   
+           
+        ); 
+
+
+    }
+
+        
+}
+
+export default first;
